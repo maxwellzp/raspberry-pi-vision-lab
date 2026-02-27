7d29f0caa4b2fdc8a7a8045512e851e24aa0ab3e
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2026-02-27 12:32:57 +0200

    Code refactoring + logger

--- basics/capture_photo.py
+++ basics/capture_photo.py
@@ -3,6 +3,7 @@ from libcamera import Transform
 from datetime import datetime
 from time import sleep
 from pathlib import Path
+import logging
 
 DATE_TIME_FILE_FORMAT = "%Y%m%d_%H%M%S"
 
@@ -10,28 +11,50 @@ def build_filename(storage_dir):
     timestamp = datetime.now().strftime(DATE_TIME_FILE_FORMAT)
     return storage_dir / f"photo_{timestamp}.jpg"
 
+def setup_logging(log_file) -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
+
+
 def main():
     storage_dir = Path("storage/photos")
+    logs_dir = Path("logs")
     try:
         storage_dir.mkdir(parents=True, exist_ok=True)
     except OSError as e:
-        print("Can't create a directory")
+        print(f"Can't create storage/photos directory: {e}")
         return
-
-    camera = Picamera2()
-    config = camera.create_still_configuration(
-        transform=Transform(hflip=1, vflip=1)
-    )
-    camera.configure(config)
     filename = build_filename(storage_dir)
 
-    camera.start()
-    sleep(3)
+    try:
+        logs_dir.mkdir(parents=True, exist_ok=True)
+    except OSError as e:
+        print(f"Can't create logs directory: {e}")
+        return
+    log_file = logs_dir / "app.log"
+    setup_logging(log_file)
+    try:
+        camera = Picamera2()
 
-    camera.capture_file(str(filename))
-    camera.stop()
+        config = camera.create_still_configuration(
+        transform=Transform(hflip=1, vflip=1))
 
-    print(f"Photo saved: {filename}")
+        camera.configure(config)
+        camera.start()
+        sleep(3)
+
+        camera.capture_file(str(filename))
+        camera.stop()
+
+        logging.info(f"Photo saved: {filename}")
+    except Exception as e:
+        logging.error(f"Camera error: {e}", exc_info=True)
 
 
 if __name__ == "__main__":
